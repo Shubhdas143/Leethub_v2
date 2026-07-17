71ab2349cb4ee0cff1d0b0419443ee65cf8e817b
Shubhranshu Shekhar Das <shubhdas29004@gmail.com>
2026-07-17 14:08:46 +0530

    Time: 29 ms (87.5%), Space: 106.1 MB (6.25%) - LeetHub

---
+++ 3312-sorted-gcd-pair-queries/3312-sorted-gcd-pair-queries.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[] gcdValues(int[] nums, long[] queries) {
+        int max = 0;
+        for (int x : nums) max = Math.max(max, x);
+
+        int[] freq = new int[max + 1];
+        for (int x : nums) freq[x]++;
+
+        long[] exact = new long[max + 1];
+
+        for (int g = max; g >= 1; g--) {
+            long cnt = 0;
+            for (int m = g; m <= max; m += g) {
+                cnt += freq[m];
+            }
+            exact[g] = cnt * (cnt - 1) / 2;
+            for (int m = g + g; m <= max; m += g) {
+                exact[g] -= exact[m];
+            }
+        }
+
+        long[] prefix = new long[max + 1];
+        for (int i = 1; i <= max; i++) {
+            prefix[i] = prefix[i - 1] + exact[i];
+        }
+
+        int[] ans = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            long k = queries[i] + 1;
+            int l = 1, r = max;
+            while (l < r) {
+                int mid = (l + r) / 2;
+                if (prefix[mid] >= k) {
+                    r = mid;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            ans[i] = l;
+        }
+
+        return ans;
+    }
+}
